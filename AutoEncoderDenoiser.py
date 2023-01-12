284bb44438acc0455fc9b408ca35beaf47aed2ca
Wouter Besse <lolmaker5@gmail.com>
2023-01-12 14:16:00 +0100

    Set up general DAE structure

--- AutoEncoderDenoiser.py
+++ AutoEncoderDenoiser.py
@@ -5,17 +5,47 @@ import os
 from tqdm import tqdm
 import numpy as np
 
+class Encoder(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        
+
+    def forward(self, x):
+        
+
+        return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        
+
+    def forward(self, x):
+        
+
+        return x
+
+
 class SPDenoiser(nn.Module):
-    def __init__(self, features, rec_field, steps, device):
+    def __init__(self, features, rec_field, steps, latent_dim, device):
         super().__init__()
         # Input size = [c, features (60), receptive field (256)]
 
-        
+        self.encoder = Encoder()
+        self.decoder = Decoder()
 
 
         self.device = device
 
         
     def forward(self, input):
+        latent_rep = self.encoder(input)
+        clean_rep = self.decoder(latent_rep)
 
-        return x
+
+        return clean_rep
